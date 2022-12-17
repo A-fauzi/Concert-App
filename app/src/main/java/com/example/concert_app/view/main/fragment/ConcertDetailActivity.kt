11ea83e13f2fedb761a4194b0a350b3b2468780c
A-fauzi <akhmadfauzi0101@gmail.com>
2022-12-17 19:21:03 +0700

    Create Mapbox Camera

--- app/src/main/java/com/example/concert_app/view/main/fragment/ConcertDetailActivity.kt
+++ app/src/main/java/com/example/concert_app/view/main/fragment/ConcertDetailActivity.kt
@@ -6,6 +6,13 @@ import android.util.Log
 import androidx.core.net.toUri
 import com.example.concert_app.R
 import com.example.concert_app.databinding.ActivityConcertDetailctivityBinding
+import com.mapbox.mapboxsdk.Mapbox
+import com.mapbox.mapboxsdk.camera.CameraPosition
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
+import com.mapbox.mapboxsdk.geometry.LatLng
+import com.mapbox.mapboxsdk.geometry.LatLngBounds
+import com.mapbox.mapboxsdk.maps.MapView
+import com.mapbox.mapboxsdk.maps.Style
 import com.squareup.picasso.Picasso
 import java.lang.Exception
 
@@ -13,14 +20,72 @@ class ConcertDetailActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityConcertDetailctivityBinding
 
+
+    private var mapView: MapView? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Mapbox.getInstance(this, getString(R.string.access_token))
 
         binding = ActivityConcertDetailctivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         retrieveData()
 
+        mapView = findViewById<MapView>(R.id.mapView)
+        mapView?.onCreate(savedInstanceState)
+        mapView?.getMapAsync { mapBoxMap ->
+            val currentCameraPosition = mapBoxMap.cameraPosition
+            currentCameraPosition.zoom
+            currentCameraPosition.tilt
+
+            val position = CameraPosition.Builder()
+                .target(LatLng( -6.184655825881868,106.81210251827447))
+                .zoom(10.0)
+                .tilt(20.0)
+                .build()
+
+            mapBoxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 3000)
+
+            val latLngBounds = LatLngBounds.Builder()
+                .include(LatLng(-6.184655825881868,106.81210251827447))
+                .include(LatLng(-6.248820935608907, 106.96522445759))
+                .build()
+            mapBoxMap.animateCamera(CameraUpdateFactory.newLatLngBounds(latLngBounds, 10), 3000)
+            mapBoxMap.setStyle(Style.MAPBOX_STREETS) {
+
+            }
+        }
+
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView?.onDestroy()
     }
 
     private fun retrieveData() {
--- app/src/main/java/com/example/concert_app/view/main/fragment/TicketFragment.kt
+++ app/src/main/java/com/example/concert_app/view/main/fragment/TicketFragment.kt
@@ -10,7 +10,7 @@ import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.maps.MapView
 import com.mapbox.mapboxsdk.maps.Style
 
-private var mapView: MapView? = null
+
 
 class TicketFragment : Fragment() {
 
@@ -18,44 +18,12 @@ class TicketFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Mapbox.getInstance(requireActivity(), getString(R.string.access_token))
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ticket, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mapView = view.findViewById<MapView>(R.id.mapView)
-        mapView?.onCreate(savedInstanceState)
-        mapView?.getMapAsync { mapBoxMap ->
-            mapBoxMap.setStyle(Style.MAPBOX_STREETS) {
-
-            }
-        }
     }
 
-    override fun onStart() {
-        super.onStart()
-        mapView?.onStart()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mapView?.onStop()
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        mapView?.onSaveInstanceState(outState)
-    }
-
-    override fun onLowMemory() {
-        super.onLowMemory()
-        mapView?.onLowMemory()
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        mapView?.onDestroy()
-    }
 }
